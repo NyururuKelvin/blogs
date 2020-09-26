394153c4ec64a51246763b951e383ab1ea4c3dac
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-26 22:04:28 +0300

    my commit add classes

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class User(UserMixin,db.Model):
     username=db.Column(db.String(255),unique=True,nullable=False)
     email = db.Column(db.String(255),unique = True,index = True)
     password_hash=db.Column(db.String(255))
+    post = db.relationship('Post', backref = 'user', lazy = 'dynamic')
 
     password_secure = db.Column(db.String(255))
 
@@ -31,6 +32,47 @@ class User(UserMixin,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
+
+class Post(db.Model):
+    
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    description = db.Column(db.String(), index = True)
+    topic = db.Column(db.String())
+    category = db.Column(category_enum, server_default='product')
+    date = db.Column(db.DateTime, nullable=False, default=arrow.utcnow().datetime)
+    comments = db.relationship('Comment', backref='postss', lazy='dynamic')
+
+    def __repr__(self):
+        return f'Post {self.description}'
+    
+
+    @classmethod
+    def get_posta(cls,owner_id):
+        posts = Post.query.filter_by(owner_id=owner_id).all()
+        return posts  
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer,primary_key = True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+    comment = db.Column(db.Text)
+      
+    def __repr__(self):
+        return f"Comment('{self.user}', '{self.comment}')'"
+    
+    def save_comment(self):
+
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_coments(cls,post_id):
+        Comment = Comment.query.filter_by(post_id=post_id).all()
+        return Comment 
 
 class Blog:
     def __init__(self, id, author, quote, permalink):
