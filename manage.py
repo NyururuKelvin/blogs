063a81e48f6c7f41ac19db5ea3bb1f356cd9b12f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-26 16:45:23 +0300

    my commit set up

---
+++ manage.py
@@ -0,0 +1,22 @@
+from app import create_app
+from flask_script import Manager,Server
+
+
+# Creating app instance
+# app = create_app('test')
+app=create_app('development')
+
+manager=Manager(app)
+manager.add_command('server',Server)
+
+
+#use the manager shell decorator to access the shell on the command line
+
+@manager.command
+def test():
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
+if __name__=='__main__':
+    manager.run()
