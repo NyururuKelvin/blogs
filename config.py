179728a1fab48439d0c822124bfc21f9bbe188b4
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-29 21:09:14 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from flask_mail import Mail
 
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/blogs'
   MAIL_SERVER = 'smtp.gmail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
